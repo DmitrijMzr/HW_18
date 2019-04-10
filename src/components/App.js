a8734313f037e157f42d02fa2b5172b057bd18ac
AlexeyVed <alexey.vedkal@gmail.com>
2019-04-10 14:09:40 +0300

    structure of project

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import '../styles/App.css';
+import '../styles/App.less';
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>React App.</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
