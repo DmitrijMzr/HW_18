590e75c08ed7b6862bdfc37a62438200a01a5f17
dmitrijMzr <dmtr.ma@gmail.com>
2019-04-10 17:37:37 +0300

    1.2

---
+++ src/components/inputAndLabel/input/placeHolder.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import './topContainer.less';
+
+
+class TopContainer extends Component {
+    render() {
+        return (
+            <div class={'topContainer'}>
+                Top Bar
+            </div>
+        );
+    }
+}
+
+export default TopContainer;
