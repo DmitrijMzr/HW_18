74cf8097e92cd442a3992816d93dd63b534cc2c9
igor-ex <igorfado@gmail.com>
2019-04-13 16:17:12 +0300

    moved buttons to a component

--- src/components/toDoList/itemToDo.jsx
+++ src/components/toDoList/itemToDo.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Buttons from './buttons/buttons'
 
 class ItemToDo extends Component {
 
@@ -21,7 +22,6 @@ class ItemToDo extends Component {
     onChange = (e) => this.setState({ [e.target.name]: e.target.value })
 
     saveItem = () => {
-
         const index = parseInt(this.props.index);
         this.setState(()=>({renameItem: this.props.item.value}));
         this.props.saveItem(index, this.state.renameItem);
@@ -35,9 +35,6 @@ class ItemToDo extends Component {
 
     render() {
         let todoClass = this.props.item.done ? "done" : "undone";
-
-        let toggleButton1;
-        let toggleButton2;
         let toggleTitle;
 
         if(!this.state.isEdit) {
@@ -54,30 +51,17 @@ class ItemToDo extends Component {
                           </div>
         }
 
-        if(!this.state.isEdit) {
-            toggleButton1 = <button type="button"
-                                   className="todo-list__item-bttn_bttn-edit"
-                                   onClick={this.editItem}>Edit</button>
-            toggleButton2 = <button type="button"
-                                    className="todo-list__item-bttn_bttn-del"
-                                    onClick={this.onClickClose}>&times;</button>
-        } else {
-            toggleButton1 = <button type="button"
-                                   className="todo-list__item-bttn_bttn-save"
-                                   onClick={this.saveItem}>Save</button>
-            toggleButton2 = <button type="button"
-                                    className="todo-list__item-bttn_bttn-del"
-                                    onClick={this.onClickClose} disabled>&times;</button>
-
-        }
-
         return (
             <div className={'todo-list__item'}>
                 <div className={todoClass} aria-hidden="true">
                     {toggleTitle}
                     <div className="todo-list__item-bttn">
-                        {toggleButton1}
-                        {toggleButton2}
+                        <Buttons
+                            isEdit = {this.state.isEdit}
+                            editCallback = {this.editItem}
+                            saveCallback = {this.saveItem}
+                            closeCallback = {this.onClickClose}
+                        />
                     </div>
                 </div>
             </div>
